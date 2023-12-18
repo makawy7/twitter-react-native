4398c528a068d9cc5b6757302898254a5252ae0c
abdallah <abdallahelmekawy6@gmail.com>
2023-12-18 19:50:30 +0200

    wrapping up home screen

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,21 +1,222 @@
-import { Button, Text, View } from 'react-native';
+import {
+  Button,
+  FlatList,
+  Image,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { EvilIcons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+
+const data = [
+  {
+    id: '1',
+    title: 'First Item',
+  },
+  {
+    id: '2',
+    title: 'Second Item',
+  },
+  {
+    id: '3',
+    title: 'Third Item',
+  },
+  {
+    id: '4',
+    title: 'Fourth Item',
+  },
+  {
+    id: '5',
+    title: 'Fifth Item',
+  },
+  {
+    id: '6',
+    title: 'Sixth Item',
+  },
+  {
+    id: '7',
+    title: 'Seventh Item',
+  },
+  {
+    id: '8',
+    title: 'Eight Item',
+  },
+  {
+    id: '9',
+    title: 'Ninth Item',
+  },
+  {
+    id: '10',
+    title: 'Tenth Item',
+  },
+];
 
 export default function HomeScreen({ navigation }) {
+  const goToProfile = () => {
+    navigation.navigate('Profile Screen');
+  };
+  const goToSingleTweet = () => {
+    navigation.navigate('Tweet Screen');
+  };
+  const goToNewTweet = () => {
+    navigation.navigate('New Tweet');
+  };
+
+  const renderItem = ({ item }) => (
+    <View style={styles.tweetContainer}>
+      <TouchableOpacity onPress={() => goToProfile()}>
+        <Image
+          style={styles.avatar}
+          source={{
+            uri: 'https://reactnative.dev/img/tiny_logo.png',
+          }}
+        />
+      </TouchableOpacity>
+      <View style={{ flex: 1 }}>
+        <TouchableOpacity
+          style={styles.flexRow}
+          onPress={() => goToSingleTweet()}
+        >
+          <Text numberOfLines={1} style={styles.tweetName}>
+            {item.title}
+          </Text>
+          <Text numberOfLines={1} style={styles.tweetHandle}>
+            @username
+          </Text>
+          <Text>&middot;</Text>
+          <Text numberOfLines={1} style={styles.tweetHandle}>
+            9m
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.tweetContentContainer}
+          onPress={() => goToSingleTweet()}
+        >
+          <Text style={styles.tweetContent}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam aut
+            vel modi quia maxime doloribus ipsa mollitia, eum at eveniet.
+          </Text>
+        </TouchableOpacity>
+
+        <View style={styles.tweetEngagement}>
+          <TouchableOpacity style={styles.flexRow}>
+            <EvilIcons
+              name="comment"
+              size={22}
+              color="gray"
+              style={{ marginRight: 2 }}
+            />
+            <Text style={styles.textGray}>32</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.flexRow}>
+            <EvilIcons
+              name="retweet"
+              size={22}
+              color="gray"
+              style={{ marginRight: 2 }}
+            />
+            <Text style={styles.textGray}>5,456</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.flexRow}>
+            <EvilIcons
+              name="heart"
+              size={22}
+              color="gray"
+              style={{ marginRight: 2 }}
+            />
+            <Text style={styles.textGray}>456</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.flexRow}>
+            <EvilIcons
+              name={Platform.OS === 'ios' ? 'share-apple' : 'share-google'}
+              size={22}
+              color="gray"
+              style={{ marginRight: 2 }}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home Screen / Feed</Text>
-      <Button
-        title="New Tweet"
-        onPress={() => navigation.navigate('New Tweet')}
+    <View style={styles.container}>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        ItemSeparatorComponent={() => (
+          <View style={styles.tweetSeperator}></View>
+        )}
       />
-      <Button
-        title="Tweet Screen"
-        onPress={() => navigation.navigate('Tweet Screen')}
-      />
-      <Button
-        title="Profile Screen"
-        onPress={() => navigation.navigate('Profile Screen')}
-      />
+      <TouchableOpacity
+        onPress={() => goToNewTweet()}
+        style={styles.floatingButton}
+      >
+        <AntDesign name="plus" size={26} color="white" />
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  flexRow: {
+    flexDirection: 'row',
+  },
+  textGray: {
+    color: 'gray',
+  },
+  tweetContainer: {
+    flexDirection: 'row',
+    paddingHorizontal: 12,
+    paddingVertical: 12,
+  },
+  tweetSeperator: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#e5e7eb',
+  },
+  avatar: {
+    width: 42,
+    height: 42,
+    marginRight: 8,
+    borderRadius: 21,
+  },
+  tweetName: {
+    fontWeight: 'bold',
+    marginRight: 2,
+    color: '#222222',
+  },
+  tweetHandle: {
+    marginHorizontal: 6,
+  },
+  tweetContentContainer: {
+    marginTop: 4,
+  },
+  tweetContent: {
+    lineHeight: 20,
+  },
+  tweetEngagement: {
+    marginTop: 12,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 14,
+  },
+  floatingButton: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#1d9bf1',
+    position: 'absolute',
+    right: 12,
+    bottom: 20,
+  },
+});
